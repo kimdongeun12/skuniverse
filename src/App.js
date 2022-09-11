c543bdb8b1bc7ee5b1091cacee43501d7b5d3026
dongeun <kdedong12@gmail.com>
2022-09-11 11:23:28 +0900

    아이콘 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
+import React from 'react';
+import {View} from 'react-native';
 import {createAppContainer} from 'react-navigation';
 import {createBottomTabNavigator} from 'react-navigation-tabs';
+import Icon from 'react-native-vector-icons/Ionicons';
 import Main from './Pages/Main/Main';
 import Alignment from './Pages/Alignment/Alignment';
 import Maps from './Pages/Maps/Maps';
@@ -9,16 +12,62 @@ const TabNavigator = createBottomTabNavigator(
   {
     Main: {
       screen: Main,
+      navigationOptions: {
+        tabBarLabel: '홈',
+        tabBarIcon: ({tintColor}) => (
+          <View>
+            <Icon style={[{color: tintColor}]} size={25} name={'home-outline'} />
+          </View>
+        ),
+        activeColor: '#DC143C',
+        inactiveColor: '#226557',
+        barStyle: {backgroundColor: '#FFC0CB'},
+      },
     },
     Alignment: {
       screen: Alignment,
+      navigationOptions: {
+        tabBarLabel: '맞춤검색',
+        tabBarIcon: ({tintColor}) => (
+          <View>
+            <Icon style={[{color: tintColor}]} size={25} name={'filter-outline'} />
+          </View>
+        ),
+        activeColor: '#DC143C',
+        inactiveColor: '#226557',
+        barStyle: {backgroundColor: '#FFC0CB'},
+      },
     },
     Maps: {
       screen: Maps,
+      navigationOptions: {
+        tabBarLabel: '지도검색',
+        tabBarIcon: ({tintColor}) => (
+          <View>
+            <Icon style={[{color: tintColor}]} size={25} name={'map-outline'} />
+          </View>
+        ),
+        activeColor: '#DC143C',
+        inactiveColor: '#226557',
+        barStyle: {backgroundColor: '#FFC0CB'},
+      },
     },
     Mypage: {
       screen: Mypage,
+      navigationOptions: {
+        tabBarLabel: '마이페이지',
+        tabBarIcon: ({tintColor}) => (
+          <View>
+            <Icon style={[{color: tintColor}]} size={25} name={'person-circle'} />
+          </View>
+        ),
+        activeColor: '#DC143C',
+        inactiveColor: '#226557',
+        barStyle: {backgroundColor: '#FFC0CB'},
+      },
     }
-});
+  }
+  
+);
 
 export default createAppContainer(TabNavigator);
