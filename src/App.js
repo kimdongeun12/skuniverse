7eaa5788d4bf93ed76fe02abd0e53495e1141f6e
dongeun <kdedong12@gmail.com>
2022-09-11 14:22:46 +0900

    tab 수정

--- src/App.js
+++ src/App.js
@@ -1,73 +1,62 @@
 import React from 'react';
-import {View} from 'react-native';
-import {createAppContainer} from 'react-navigation';
-import {createBottomTabNavigator} from 'react-navigation-tabs';
+import {NavigationContainer} from '@react-navigation/native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {Text} from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons';
 import Main from './Pages/Main/Main';
 import Alignment from './Pages/Alignment/Alignment';
 import Maps from './Pages/Maps/Maps';
 import Mypage from './Pages/Mypage/Mypage';
 
-const TabNavigator = createBottomTabNavigator(
-  {
-    Main: {
-      screen: Main,
-      navigationOptions: {
-        tabBarLabel: '홈',
-        tabBarIcon: ({tintColor}) => (
-          <View>
-            <Icon style={[{color: tintColor}]} size={25} name={'home-outline'} />
-          </View>
-        ),
-        activeColor: '#DC143C',
-        inactiveColor: '#226557',
-        barStyle: {backgroundColor: '#FFC0CB'},
-      },
-    },
-    Alignment: {
-      screen: Alignment,
-      navigationOptions: {
-        tabBarLabel: '맞춤검색',
-        tabBarIcon: ({tintColor}) => (
-          <View>
-            <Icon style={[{color: tintColor}]} size={25} name={'filter-outline'} />
-          </View>
-        ),
-        activeColor: '#DC143C',
-        inactiveColor: '#226557',
-        barStyle: {backgroundColor: '#FFC0CB'},
-      },
-    },
-    Maps: {
-      screen: Maps,
-      navigationOptions: {
-        tabBarLabel: '지도검색',
-        tabBarIcon: ({tintColor}) => (
-          <View>
-            <Icon style={[{color: tintColor}]} size={25} name={'map-outline'} />
-          </View>
-        ),
-        activeColor: '#DC143C',
-        inactiveColor: '#226557',
-        barStyle: {backgroundColor: '#FFC0CB'},
-      },
-    },
-    Mypage: {
-      screen: Mypage,
-      navigationOptions: {
-        tabBarLabel: '마이페이지',
-        tabBarIcon: ({tintColor}) => (
-          <View>
-            <Icon style={[{color: tintColor}]} size={25} name={'person-circle'} />
-          </View>
-        ),
-        activeColor: '#DC143C',
-        inactiveColor: '#226557',
-        barStyle: {backgroundColor: '#FFC0CB'},
-      },
-    }
-  }
-  
-);
+const Tab = createBottomTabNavigator();
 
-export default createAppContainer(TabNavigator);
+function BottomTabNavigationApp() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator initialRouteName="Home">
+        <Tab.Screen
+          name="Home"
+          component={Main}
+          options={{
+            title: '홈',
+            tabBarIcon: ({color, size}) => (
+              <Icon name="home-outline" color={color} size={size} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Alignment"
+          component={Alignment}
+          options={{
+            title: '맞춤검색',
+            tabBarIcon: ({color, size}) => (
+              <Icon name="filter-outline" color={color} size={size} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Maps"
+          component={Maps}
+          options={{
+            title: '지도검색',
+            tabBarIcon: ({color, size}) => (
+              <Icon name="map-outline" color={color} size={size} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Mypage"
+          component={Mypage}
+          options={{
+            title: '마이페이지',
+            tabBarIcon: ({color, size}) => (
+              <Icon name="person-circle" color={color} size={size} />
+            ),
+          }}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default BottomTabNavigationApp;
