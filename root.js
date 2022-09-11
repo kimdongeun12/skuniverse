405a39a49cd34add5cebb9d201d984a4ebe7a4b4
w00cheol <kw8384@gmail.com>
2022-09-11 22:16:14 +0900

    CREATE detail

---
+++ root.js
@@ -0,0 +1,20 @@
+// import React from 'react';
+// import Main from './src/Pages/Main/Main';
+// import {NavigationContainer} from 'react-native';
+// import Detailpage from './src/Pages/Detailpage/Detailpage';
+// import Detailpage2 from './src/Pages/Detailpage/Detailpage2';
+// import { createStackNavigator } from '@react-navigation/stack';
+// const Stack = createStackNavigator();
+
+// function Rootpage({}) {
+//   return (
+//     <NavigationContainer>
+//       <Stack.Navigator initialRouteName="Detailpage">
+//         <Stack.Screen name="Detailpage" component={Detailpage} />
+//         <Stack.Screen name="Detailpage2" component={Detailpage2} />
+//       </Stack.Navigator>
+//     </NavigationContainer>
+//   );
+// }
+
+// export default Rootpage;
