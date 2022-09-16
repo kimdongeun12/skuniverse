54f01b28a3e51424ec6265dcb4f96d81952adac5
박주영 <yhs07240@naver.com>
2022-09-17 04:59:04 +0900

    9.17 버튼 기초

--- src/Screen/Alignment/Alignment.js
+++ src/Screen/Alignment/Alignment.js
@@ -1,15 +1,39 @@
 
-import React from 'react';
-import {Text , View} from 'react-native';
+import React, { useState,useEffect }  from 'react';
+import {Text , View,  Alert, StyleSheet,SafeAreaView , Button , TouchableOpacity, FlatList , Image , Dimensions} from 'react-native';
 import styled from "styled-components";
+import { Chip} from 'react-native-paper';
+import axios from 'axios';
 
-function Alignment() {
+const Alignment = () => {
+  const [select, setSelect] = useState(false);
   return (
-    <AligmentWrap>
-      <Text>Alignment</Text>
-    </AligmentWrap>
+    
+    <View>
+    <View style={styles.chip}>
+      <Chip
+        icon={select ? "check-bold" : "information"}
+        style={styles.closeChip}
+        selected={select}
+        selectedColor={select ? 'skyblue' : 'black'}
+        onClose={() => console.log('Close Chip')}
+        onPress={() => setSelect(!select)}>
+        10대
+      </Chip>
+    </View>
+    </View>
   );
-}
+  };
+
+  
+  const styles = StyleSheet.create({
+  chip: {
+    width: 100,
+    marginLeft: 20,
+    marginTop: 20,
+    marginBottom: 10,
+  },
+  });
 
 const AligmentWrap = styled.View`
   flex: 1;
