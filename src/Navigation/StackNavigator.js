2482825d9eed2c820861a29f7f273ddd8b3a9f1a
haerim <haerim0228@naver.com>
2022-09-19 06:56:18 +0900

    ADD Homepage

--- src/Navigation/StackNavigator.js
+++ src/Navigation/StackNavigator.js
@@ -1,6 +1,8 @@
 // ./navigation/StackNavigator.js
 
-import React from "react";
+import React, { Component } from "react";
+import { View, Image, StyleSheet, Text } from 'react-native';
+
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 // import Icon from 'react-native-vector-icons/Ionicons';
@@ -17,6 +19,13 @@ import MypageScreen from '../Screen/Mypage/Mypage';
 
 import LoginScreen from '../Screen/Login/Login';            // hong-da-hyeon 작업
 
+import Arrta from 'skuniverse/Assets/images/Arrta.png';
+//import Cheol from 'skuniverse/Assets/images/Cheol.jpg';
+import Card3 from 'skuniverse/Assets/images/Card3.jpg';
+
+
+
+
 const Stack = createNativeStackNavigator();
 const HomeStack = createNativeStackNavigator();
 const AlignmentStack = createNativeStackNavigator();
@@ -24,12 +33,27 @@ const MapsStack = createNativeStackNavigator();
 const MypageStack = createNativeStackNavigator();
 const LoginStack = createNativeStackNavigator();      // hong-da-hyeon 작업
 
+class LogoTitle extends Component {
+  render() {
+      return (
+        <View>
+          <Image 
+              style={{ width:100, height:70 }}   
+              source={Arrta}  
+              resizeMode='contain'                           
+          />
+          </View>
+      )
+  }
+
+}
 const screenOptionStyle = {
   headerStyle: {
-    backgroundColor: "#9AC4F8",
+    backgroundColor: "white",
   },
   headerTintColor: "white",
   headerBackTitle: "Back",
+  headerTitle:  props => <LogoTitle {...props} />
 };
 
 
@@ -40,10 +64,10 @@ const LoginStackNavigator = () => {
       <LoginStack.Screen 
         name="Login" 
         component={LoginScreen} 
-        options={({navigation, route}) => ({
-          headerLeft: null,
-          headerTitle: '로그인',
-        })}
+        // options={({navigation, route}) => ({
+        //   headerLeft: null,
+        //   headerTitle: '로그인',
+        // })}
         
       />
     </Stack.Navigator>
@@ -56,10 +80,10 @@ const HomeStackNavigator = () => {
       <HomeStack.Screen 
         name="Home" 
         component={HomeScreen} 
-        options={({navigation, route}) => ({
-          headerLeft: null,
-          headerTitle: '앱이름',
-        })}
+        // options={({navigation, route}) => ({
+        //   headerLeft: null,
+        //   headerTitle: '홈',
+        // })}
       />
     </Stack.Navigator>
   );
@@ -71,10 +95,10 @@ const AlignmentStackNavigator = () => {
       <AlignmentStack.Screen 
         name="Alignment" 
         component={AlignmentScreen} 
-        options={({navigation, route}) => ({
-          headerLeft: null,
-          headerTitle: '앱이름',
-        })}
+        // options={({navigation, route}) => ({
+        //   headerLeft: null,
+        //   headerTitle: '앱이름',
+        // })}
         
       />
     </Stack.Navigator>
@@ -87,10 +111,10 @@ const MapsStackNavigator = () => {
       <MapsStack.Screen
         name="Maps"
         component={MapsScreen}
-        options={({navigation, route}) => ({
-          headerLeft: null,
-          headerTitle: '앱이름',
-        })}
+        // options={({navigation, route}) => ({
+        //   headerLeft: null,
+        //   headerTitle: '앱이름',
+        // })}
       />
       <MapsStack.Screen
         name="MapsLists"
@@ -118,14 +142,14 @@ const MypageStackNavigator = () => {
       <MypageStack.Screen 
         name="Mypage" 
         component={MypageScreen} 
-        options={({navigation, route}) => ({
-          headerLeft: null,
-          headerTitle: 'skuniverse 님',
-        })}
+        // options={({navigation, route}) => ({
+        //   headerLeft: null,
+        //   headerTitle: 'skuniverse 님',
+        // })}
       />
     </Stack.Navigator>
   );
 }
 
 // LoginStackNavigator 추가 : hong-da-hyeon 작업
-export { LoginStackNavigator, HomeStackNavigator, AlignmentStackNavigator ,MapsStackNavigator , MypageStackNavigator };
+export { LoginStackNavigator, HomeStackNavigator, AlignmentStackNavigator ,MapsStackNavigator , MypageStackNavigator};
--- src/Screen/Home/Home.js
+++ src/Screen/Home/Home.js
@@ -1,21 +1,84 @@
-
 import React from 'react';
-import {Text , View} from 'react-native';
-import styled from "styled-components";
+import { SafeAreaView, 
+  View, 
+  Image, 
+  StyleSheet, 
+  ScrollView, 
+  Text, 
+  TouchableOpacity, 
+  Linking } from 'react-native';
 
-function Main({navigation}) {
-  return (
-    <MainWrap>
-      <Text>Main</Text>
-    </MainWrap>
-  );
+import { SliderBox } from 'react-native-image-slider-box';
+import Card1 from 'skuniverse/Assets/images/Card1.jpg';
+import Card2 from 'skuniverse/Assets/images/Card2.jpg';
+import Card3 from 'skuniverse/Assets/images/Card3.jpg';
+
+
+
+
+function Home() {
+  return ( <SafeAreaView style={styles.container}>
+      <ScrollView style={styles.scrollView}>
+        <SliderBox style={styles.slider}
+          images={[
+            "http://www.naruart.or.kr/data/editor/2209/thumb-d981745cec2e6b1ec7eb71e7c80f4964_1662440580_7656_835x1181.jpg",
+            "https://www.gangnam.go.kr/upload/editor/2022/07/15/b7770c87-4871-4519-873f-c4e6bab3b4df.jpg",
+            "http://www.naruart.or.kr/data/editor/2209/thumb-783369461a8a681ad2294ca894f8a298_1662444664_0958_835x1178.jpg"
+          ]}
+          autoplay
+          circleLoop /> 
+        <Text style={styles.text}>
+          수고하셨어요. 우철선생님.
+        </Text>
+        <TouchableOpacity style = {styles.card1}
+        onPress = {() => Linking.openURL('https://www.mnuri.kr/munhwa/introduceNuri.do')}>
+        <Image source = {Card1} style = {styles.card1}/> 
+        </TouchableOpacity> 
+        <TouchableOpacity style = {styles.card2}
+        onPress = {() => Linking.openURL('https://www.mnuri.kr/munhwa/cardIssueGuide.do')}>
+        <Image source = {Card2} style = {styles.card2}/>
+        </TouchableOpacity> 
+        <TouchableOpacity style = {styles.card3}
+        onPress = {() => Linking.openURL('https://www.mnuri.kr/help/customerService/faq.do')}>
+        <Image source = {Card3} style = {styles.card3}/>
+        </TouchableOpacity>
+      </ScrollView>
+    </SafeAreaView>
+  )
 }
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 10,
+    backgroundColor: 'white',
+  },
+  slider: {
+    width: 380,
+    height: 600,
+    resizeMode: 'contain',
+  },
+  scrollView: {
+    backgroundColor: 'white',
+  },
+  text: {
+    fontSize: 30
+  },
+  card1: {
+    flex: 1,
+    width: 370, 
+    height: 450
+  },
+  card2: {
+    flex: 1,
+    width: 380, 
+    height: 360
+  },
+  card3: {
+    flex: 1,
+    width: '100%', 
+    height: 170
+  },
+});
 
-const MainWrap = styled.View`
-  flex: 1;
-  justify-content: center;
-  align-items: center;
-`
-
-export default Main;
+export default Home;
