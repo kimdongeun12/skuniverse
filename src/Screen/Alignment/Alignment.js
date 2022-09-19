e01242ff128ad4b6c7a8c1f17a60147e8b718cba
박주영 <yhs07240@naver.com>
2022-09-19 21:01:06 +0900

    9.19 useState

--- src/Screen/Alignment/Alignment.js
+++ src/Screen/Alignment/Alignment.js
@@ -1,39 +1,72 @@
 
 import React, { useState,useEffect }  from 'react';
-import {Text , View,  Alert, StyleSheet,SafeAreaView , Button , TouchableOpacity, FlatList , Image , Dimensions} from 'react-native';
+import {Text , View,  Alert, StyleSheet,SafeAreaView  , TouchableOpacity, FlatList , Image , Dimensions, ScrollView, StatusBar} from 'react-native';
 import styled from "styled-components";
-import { Chip} from 'react-native-paper';
+import { Chip, Button } from 'react-native-paper';
+
 import axios from 'axios';
 
 const Alignment = () => {
-  const [select, setSelect] = useState(false);
+  const [agegroup, setagegroup] = useState(0);
+
+  
   return (
     
     <View>
-    <View style={styles.chip}>
-      <Chip
-        icon={select ? "check-bold" : "information"}
-        style={styles.closeChip}
-        selected={select}
-        selectedColor={select ? 'skyblue' : 'black'}
-        onClose={() => console.log('Close Chip')}
-        onPress={() => setSelect(!select)}>
-        10대
-      </Chip>
-    </View>
+      <View style={styles.row}>
+
+        <Button style={styles.btnset} mode="contained" onPress={() => {setagegroup(10); console.log(agegroup)}}>
+        <Text style={styles.chipText}>10대</Text>
+        </Button>
+
+        <Button style={styles.btnset} mode="contained" onPress={() => {setagegroup(20); console.log(agegroup)}}>
+        <Text style={styles.chipText}>20대</Text>
+        </Button>
+
+        <Button style={styles.btnset} mode="contained" onPress={() => {setagegroup(30); console.log(agegroup)}}>
+        <Text style={styles.chipText}>30대</Text>
+        </Button>
+
+        <Button style={styles.btnset} mode="contained" onPress={() => {setagegroup(40); console.log(agegroup)}}>
+        <Text style={styles.chipText}>40대</Text>
+        </Button>
+
+        <Button style={styles.btnset} mode="contained" onPress={() => {setagegroup(50); console.log(agegroup)}}>
+        <Text style={styles.chipText}>50대</Text>
+        </Button>
+
+        <Button style={styles.btnset} mode="contained" onPress={() => {setagegroup(60); console.log(agegroup)}}>
+        <Text style={styles.chipText}>60대</Text>
+        </Button>
+      </View>
+
     </View>
   );
   };
 
-  
-  const styles = StyleSheet.create({
-  chip: {
-    width: 100,
-    marginLeft: 20,
-    marginTop: 20,
-    marginBottom: 10,
+
+
+const styles = StyleSheet.create({
+  row: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    paddingHorizontal: 12
   },
-  });
+
+  btnset: {
+    backgroundColor: "#e0ddf0",
+    margin: 4,
+    borderRadius: 100,
+
+  },
+
+  chipText: {
+    color: "#04020f"
+  },
+
+});
+
+  
 
 const AligmentWrap = styled.View`
   flex: 1;
